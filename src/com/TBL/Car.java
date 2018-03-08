35fb4d3a130c1909b0ea3b00502ab45b3e7df094
Tom Bennett-Lloyd <36540971+TomBennett-Lloyd@users.noreply.github.com>
2018-03-08 22:16:10 +0000

    Updates

--- src/com/TBL/Car.java
+++ src/com/TBL/Car.java
@@ -2,37 +2,98 @@ package com.TBL;
 
 public class Car extends Vehicle {
     private int gear;
-    private int power;
-    private int revs;
-    private int maxRevs;
+    private float revs;
+    private float maxRevs;
+    private int acceleration;
+    private float maxSpeed;
+    private int maxGear;
 
-    public Car(String name, String size, int currdirection, int currspeed, int gear, int power,int revs,int maxRevs) {
-        super(name, size, currdirection, currspeed);
-        this.gear = gear;
-        this.power = power;
-        this.revs=revs;
-        this.maxRevs=maxRevs;
+    public Car(String name, String size, float maxSpeed, int zeroToSixty) {
+        super(name, size);
+        this.gear = 1;
+        this.acceleration=60/zeroToSixty;
+        this.revs=10000;
+        this.maxRevs=90000;
+        this.maxGear = 5;
+        this.maxSpeed = maxSpeed;
     }
 
 
 
-    public void changegears(int gears){
+    public boolean changegears(int gears){
         this.gear+=gears;
+        if (this.gear<1){
+            this.stop();
+            this.revs=10000;
+            return false;
+        } else if(this.gear<=this.maxGear) {
+            return true;
+        } else {
+            this.gear=this.maxGear;
+            return false;
+        }
 
     }
 
-    private int availablePower(){
-        int avialable = -1*(Math.abs(this.revs-this.maxRevs)/this.maxRevs)+1;
-        return avialable;
+    private void changeRevs(float throttle){
+        float revCoeff= (float)0.7;
+        float rev =this.revs+(throttle*this.maxRevs*revCoeff);
+        if (rev>this.maxRevs){
+            boolean topSpeed=this.changegears(1);
+            if (!topSpeed){
+                this.revs=this.maxRevs;
+            } else {
+                this.revs= Math.max(rev-this.maxRevs,8000);
+            }
 
+        } else if (rev<8000){
+            boolean moving=this.changegears(-1);
+            if (moving){
+                this.revs= this.maxRevs-rev;
+            }
+        } else{
+            this.revs=rev;
+        }
     }
 
 
-    public void accellerate (int throttle, int time){
-        int speedchange = throttle*this.availablePower()*time;
-        this.changeSpeed(speedchange);
+    public void accellerate (float throttle, int time){
+        if (throttle > 100) {
+            throw new IllegalArgumentException("throttle must be a number from 1 to 100") ;
+        }else{
+            throttle /= 100;
+        }
+        for (int i = 0; i < time ; i++) {
+            float speedchange = throttle*this.acceleration;
+            this.changeRevs(throttle);
+
+            if (this.changeSpeed(speedchange)>this.maxSpeed){
+                this.changeSpeed(this.maxSpeed-this.getSpeed());
+            } else {
+                this.changeSpeed(speedchange);
+            }
+
+        }
     }
 
 
+    public int getGear() {
+        return gear;
+    }
 
+    public float getRevs() {
+        return revs;
+    }
+
+    public float getMaxRevs() {
+        return maxRevs;
+    }
+
+    public int getAcceleration() {
+        return acceleration;
+    }
+
+    public float getMaxSpeed() {
+        return maxSpeed;
+    }
 }
--- src/com/TBL/Main.java
+++ src/com/TBL/Main.java
@@ -3,6 +3,17 @@ package com.TBL;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car fordCMax = new Car("FordCmax","Estate",100,12);
+
+        for (int i=0;i<3;i++){
+            fordCMax.accellerate(100,2);
+            System.out.println("speed is "+fordCMax.getSpeed());
+            System.out.println("gear is "+fordCMax.getGear());
+            System.out.println("Revs are "+fordCMax.getRevs());
+            fordCMax.changeDirection(20);
+            System.out.println("Direction is "+fordCMax.getDirection());
+            System.out.println("");
+        }
+
     }
 }
